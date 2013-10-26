cf284d46c71c6f4479a3a30e14d7c802aecb4302
cliffsun91 <cliffsun91@gmail.com>
2013-10-26 11:16:28 +0100

    extra commit

--- platforms/android/assets/www/js/appcontroller.js
+++ platforms/android/assets/www/js/appcontroller.js
@@ -1,18 +1,31 @@
-$(document).ready(function() {
-  // Set the variable $width to the width of our wrapper on page load
-  var width = $(document).width();
-  //width = 200;
-  alert('width: ' + width);
-  // Target all images inside the #content. This works best if you want to ignore certain images that are part of the layout design
-  $('.span-frontpage-logo img').css({
-      // Using jQuery CSS we write the $width variable we previously specified as a pixel value. We use max-width incase the image is smaller than our viewport it won't scale it larger. Don't forget to set height to auto or else it will squish your photos.
-    'max-width' : width/1.5, 
-    'height' : 'auto',
-    'margin': '0 auto',
-    'display': 'block'
-  });
-});
+// $(document).ready(function() {
+//   // Set the variable $width to the width of our wrapper on page load
+//   var width = $(document).width();
+//   //width = 200;
+//   alert('width: ' + width);
+//   // Target all images inside the #content. This works best if you want to ignore certain images that are part of the layout design
+//   $('.span-frontpage-logo img').css({
+//       // Using jQuery CSS we write the $width variable we previously specified as a pixel value. We use max-width incase the image is smaller than our viewport it won't scale it larger. Don't forget to set height to auto or else it will squish your photos.
+//     'max-width' : width/5, 
+//     'height' : 'auto',
+//     'margin': '0 auto',
+//     'display': 'block'
+//   });
+// });
 
+// document.addEventListener("deviceready", onDeviceReady, false);
+
+// // PhoneGap is loaded and it is now safe to make calls PhoneGap methods
+// function onDeviceReady() {
+//     // Register the event listener
+//     document.addEventListener("backbutton", onBackKeyDown, false);
+// }
+
+// // Handle the back button
+// function onBackKeyDown() {
+//   console.log("INSIDE BACK BUTTON");
+//   navigator.app.exitApp();
+// }
 
 $(document).on("mobileinit", onMobileInit);
 
@@ -68,7 +81,7 @@ var jot_logout_url = "http://www.simplyjot.com/simplyjot/index.php/site/external
 var app = angular.module('jot', ['ngTouch', 'ngRoute']);
 //var jot_login_url2 = 'http://www.simplyjot.com/simplyjot/index.php/site/externalLoginMobile?';
 
-app.controller('frontpageCtrl', function ($scope, $rootScope, LoginService) {
+app.controller('frontpageCtrl', function ($scope, $rootScope, $window, LoginService) {
 
   $scope.init = function () {
     LoginService.checkLoggedIn().then(function(response) {
@@ -94,6 +107,50 @@ app.controller('frontpageCtrl', function ($scope, $rootScope, LoginService) {
     $scope.init();
   });
 
+
+  $scope.getWidth = function() {
+    return $window.innerWidth;
+  };
+
+  $scope.getHeight = function() {
+    return $window.innerHeight;
+  };
+
+  $scope.setCSSForLogoWidth = function() {
+    alert('width: ' + $scope.windowWidth);
+
+    $('.span-frontpage-logo img').css({
+        // Using jQuery CSS we write the $width variable we previously specified as a pixel value. We use max-width incase the image is smaller than our viewport it won't scale it larger. Don't forget to set height to auto or else it will squish your photos.
+      'max-width' : $scope.windowWidth/1.5, 
+      'height' : 'auto',
+      'margin': '0 auto',
+      'display': 'block'
+    });
+  };
+
+  $scope.setCSSForLogoHeight = function() {
+    alert('height: ' + $scope.windowHeight);
+    
+    $('.div-frontpage-logo').css({
+      'margin-top' : $scope.windowHeight/6
+    });
+  };
+
+  $scope.$watch($scope.getWidth, function(newValue, oldValue) {
+    $scope.windowWidth = newValue;
+    $scope.setCSSForLogoWidth();
+    $scope.setCSSForSpinnerWidth();
+  });
+
+  $scope.$watch($scope.getHeight, function(newValue, oldValue) {
+    $scope.windowHeight = newValue;
+    $scope.setCSSForLogoHeight();
+  });
+
+  $window.onresize = function(){
+    $scope.$apply();  
+  }
+
 });
 
 
@@ -101,11 +158,10 @@ app.controller('LoginCtrl', function ($scope, $rootScope, $window, $http) {
 
   $scope.nextPage = function() {
     $.mobile.changePage("#mainpage", {transition: "slide"});
-    //$window.location.hash = '#mainpage';
   };
 
   $scope.loginExternal = function(method) {
-    var ref = $window.open(jot_login_url + method, '_blank', 'location=no');
+    var ref = $window.open(jot_login_url + method, '_blank', 'location=no,clearcache=yes,clearsessioncache=yes');
 
     ref.addEventListener('loadstop',  function(event){
       var code = '(function(){\n' +
@@ -115,23 +171,69 @@ app.controller('LoginCtrl', function ($scope, $rootScope, $window, $http) {
       if((event.url).indexOf(jot_login_url + method) != -1) {
         ref.executeScript({code: code}, function(results) {
           ref.close();
-          alert("result is: " + results);
-          if (results == "success"){
-            $rootScope.$broadcast('getAllPosts');
-            $.mobile.changePage("#mainpage", {transition: "slide"});
-          }  
-          else if (results == "not authenticated") {
-            //do something here
-          }      
-          else {
-            console.log("Error: " + results);
+          if (results && results.length === 1){
+            var data =  JSON.parse(results[0]);
+            // alert("result is: " + data);
+            // alert("status is: " + data.status);
+            // alert("email is: " + data.email);
+            if (data.status == "success"){
+              $window.localStorage.setItem("user_email", data.email);
+              $rootScope.$broadcast('getAllPosts');
+              $.mobile.changePage("#mainpage", {transition: "slide"});
+            }  
+            else if (results.status == "not authenticated") {
+              //do something here
+            }      
+            else {
+              console.log("Error: " + results);
+            }
+            //close the browser
           }
-          //close the browser
         });
       }
     });
+  };
+
+});
+
+
+app.controller('LogoutCtrl', function ($scope, $http, $window) {
+  
+  $scope.logout = function() {
+    var user_email = $window.localStorage.getItem("user_email");
+    var message = "Are you sure you want to log out? User: " + user_email;
+    navigator.notification.confirm(message, $scope.logoutCallback, "Logout?", ["OK", "Cancel"]);
 
   };
+
+  $scope.logoutCallback = function(buttonIndex) {
+    alert("logoutCallback called! buttonIndex: " + buttonIndex);
+
+    if(buttonIndex == 1) {
+      $http({
+        method: 'GET', 
+        url: jot_logout_url,
+      })
+      .then(function(response) {
+        console.log("Logging out: " + response.status + ", data: " + response.data);
+        alert("Logging out: " + response.status + ", data: " + response.data);
+        //$scope.posts = response.data;
+        $scope.posts = [];
+        $window.localStorage.clear();
+        $.mobile.changePage("#login", {transition: "slide", reverse: "true"});
+      }, function(errorResponse){
+        console.log("Logging out ERROR with status: " + errorResponse.status + ", data:" + errorResonse.data);
+        alert("Logging out ERROR with status: " + errorResponse.status + ", data:" + errorResonse.data);
+      });
+    }
+    else if(buttonIndex == 2) {
+      // do nothing.
+    } 
+    else {
+      // do nothing.
+    }
+  };
+
 });
 
 
@@ -243,6 +345,27 @@ app.controller('ListCtrl', function ($scope, $http, $window) {
     return count;
   };
 
+  $scope.isUserEmailSet = function() {
+    return $window.localStorage.getItem("user_email") != null;
+  };
+
+  $scope.userEmail = function() {
+    return $window.localStorage.getItem("user_email");
+  };
+
+
+  //Copy text to system-wide clipboard
+  $scope.copyToClipboard = function(post) {
+    alert("post.postContent: " + post.postContent);
+    $window.clipboardManagerCopy(
+      post.postContent,
+      function(r){alert("copy is successful")},
+      function(e){alert("error: " + e)}
+    );
+  };
+
+
+  //Delete Stuff, doesn't do anything, just UI stuff
   $scope.addToDeleteList = function(post) {
     if (!contains($scope.deletePostList, post.id)){
       $scope.deletePostList.push(post.id);
@@ -290,12 +413,56 @@ app.controller('ListCtrl', function ($scope, $http, $window) {
       }
       index = index + 1;
     });  
-
   };
 
-  //$scope.requestPosts();
-
 });
+
+
+app.directive('usSpinner', ['$window', function ($window) {
+  return {
+    scope: true,
+    link: function (scope, element, attr) {
+      scope.spinner = null;
+      
+      function stopSpinner() {
+        if (scope.spinner) {
+          scope.spinner.stop();
+          scope.spinner = null;
+        }
+      }
+      
+      //scope.$watch(attr.usSpinner, function (options) {
+        var options = {
+          lines: 11, // The number of lines to draw
+          length: 10, // The length of each line
+          width: 2, // The line thickness
+          radius: 10, // The radius of the inner circle
+          corners: 0.5, // Corner roundness (0..1)
+          rotate: 0, // The rotation offset
+          direction: 1, // 1: clockwise, -1: counterclockwise
+          color: '#000', // #rgb or #rrggbb or array of colors
+          speed: 1.4, // Rounds per second
+          trail: 45, // Afterglow percentage
+          shadow: false, // Whether to render a shadow
+          hwaccel: false, // Whether to use hardware acceleration
+          className: 'spinner', // The CSS class to assign to the spinner
+          zIndex: 2e9, // The z-index (defaults to 2000000000)
+          top: 'auto', // Top position relative to parent in px
+          left: 'auto' // Left position relative to parent in px
+        };
+
+        stopSpinner();
+        scope.spinner = new $window.Spinner(options);
+        scope.spinner.spin(element[0]);
+      //}, true);
+      
+      scope.$on('$destroy', function () {
+        stopSpinner();
+      });
+    }
+  };
+}]);
+
 
 app.directive('jqueryMobileTpl', function () {
     return {
